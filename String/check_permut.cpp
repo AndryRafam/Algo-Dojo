12be7ac97036545460394097796d02898f51e2ff
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2019-11-01 00:18:52 +0300

    Update check_permut.cpp

--- String/check_permut.cpp
+++ String/check_permut.cpp
@@ -1,35 +1,27 @@
-/* Check if one string is a permutation of the other one */
+// Check if one string is a permutation of the other one. Case sensitive. O(N) complexity, N length of the strings
 
 #include <bits/stdc++.h>
 using namespace std;
 
-class Permut {
-	public:
-		bool check_permut (string S1, string S2) {
-			if (S1.length() != S2.length())
-				return -1;
-			else {
-				sort (S1.begin(),S1.end());
-				sort (S2.begin(),S2.end());
-			}
-			if (S1 == S2)
-				return true;
-			else
-				return false;
-		}
+class Solution{
+    public:
+    static bool isPermut(string &s1, string &s2){
+        if(s1.length()!=s2.length())
+            return false;
+        unordered_map<char,int> hash;
+        for(auto i = 0; i < s1.length(); ++i){
+            hash[s1[i]]++;
+            hash[s2[i]]--;
+        }
+        for(auto &x : hash)
+            if(x.second!=0)
+                return false;
+        return true;
+    }
 };
 
-int main() {
-	Permut P;
-	string str1, str2;
-	getline(cin,str1); cin.ignore();
-	getline(cin,str2);
-	
-	if(P.check_permut(str1,str2))
-		cout << "True" << endl;
-	else
-		cout << "False" << endl;
-	return 0;
+int main(){
+	string test1 = "akka";
+	string test2 = "kaka";
+	printf("%d",Solution::isPermut(test1,test2));
 }
-	
-	
