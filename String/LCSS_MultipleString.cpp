483151b732e23cec80320ae55ccfaac956b5f2a4
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2019-11-18 16:05:08 +0300

    Create LCSS_MultipleString.cpp

---
+++ String/LCSS_MultipleString.cpp
@@ -0,0 +1,38 @@
+/* Program which returns the Longest Common Substring of the multiple string */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool compare(string &s1, string &s2){
+	return s1.size() < s2.size();
+}
+string findStem(vector<string> arr){
+	vector<string> res;
+	string s = "";
+	for(auto i = 0; i < arr[0].length(); ++i){
+		for(auto j = 1; j <= arr[0].length()-i; ++j){
+			s = arr[0].substr(i,j);
+			auto k = 1;
+			for(k = 1; k < arr.size(); ++k){
+				if(arr[k].find(s)==string::npos)
+					break;
+			}
+			if(k==arr.size())
+				res.emplace_back(s);
+		}
+	}
+	if(res.empty())
+		 return "KO";
+	else
+		return *max_element(res.begin(),res.end(),compare);
+}
+int main(){
+	ios_base::sync_with_stdio(0);
+	vector<string> test1 = {"gracefully","graceful","grace","disgraceful"};
+	vector<string> test2 = {"grace","sd","test"};
+  
+	cout << findStem(test1); // grace
+	cout << "\n";
+	cout << findStem(test2); // KO
+	return 0;
+}
