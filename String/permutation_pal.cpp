48a9f63e068611ae2c7f596ed2bc3d8878a9effc
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-04-19 01:20:13 +0300

    Add files via upload

---
+++ String/permutation_pal.cpp
@@ -0,0 +1,31 @@
+/*Check if any permutation of string is a palindrome O(N) complexity (N length of the string)
+
+ Examples: ivicc → True (permutation of civic which is a palindrome)
+           civic → True (palindrome itself)
+		   livic → False
+		   liciv → False */
+		   
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+	public:
+	static bool permut(string &s){
+		unordered_map<char,int> hash;
+		int odd_count = 0;
+		for(auto &x : s)
+			hash[x]++;
+		for(auto &itr : hash){
+			if(itr.second%2!=0)
+				odd_count+=1;
+		}
+		if(odd_count > 1)
+			return false;
+		return true;
+	}
+};
+int main(){
+	ios_base::sync_with_stdio(0);
+	string test = "ivicc"; // True
+	cout << Solution::permut(test);
+}
