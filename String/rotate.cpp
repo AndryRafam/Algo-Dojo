f90399e97578d1bd7a590deb6b00216fcb64af74
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-04-18 16:57:41 +0300

    Update rotate.cpp

--- String/rotate.cpp
+++ String/rotate.cpp
@@ -1,48 +1,40 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
+// Rotational point in a dictionnary
 
-class Rotate{
-public:
-    Rotate(void);
-    void rotation(std::vector<std::string> v1, std::vector<std::string> v2);
-    ~Rotate(void);
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+	public:
+	static vector<pair<int,string>> rotation(vector<string> &arr){
+        vector<pair<int,string>> rot;
+		for(auto i = 0; i < arr.size(); i++){
+			if(arr[i+1]>=arr[i])
+				continue;
+			else{
+				rot.emplace_back(i+1,arr[i+1]);
+                break;
+            }
+		}
+		return rot;
+	}
 };
 
-Rotate::Rotate(void){}
-
-void Rotate::rotation(std::vector<std::string> v1, std::vector<std::string> v2){
-    v2 = v1;
-    sort(v1.begin(),v1.end());
-    int index = 0;
-    for (auto i = 0; i < v1.size(); i++){
-      if (v2[i] == v1[0]){
-        std::cout << "Rotation point at index > " << index << std::endl;
-        std::cout << "Rotation element is > " << v2[i] << std::endl;
-    }
-    index++;
-  }
-}
-
-Rotate::~Rotate(void){}
-
 int main(void){
-  Rotate *R;
-  std::vector<std::string> v1 {
-    "polymorphe",
-    "ptolemaic",
+	ios_base::sync_with_stdio(0);
+	cout.tie(0);
+	vector<string> test = {
+	"ptolemaic",
     "retrograde",
     "supplant",
     "undulate",
     "xenoepist",
-    "asymptote",
+    "asymptote",  // <-- rotates here!
     "babka",
     "banoffee",
     "engender",
     "karpatka",
-    "othellolagkage",
-  };
-  std::vector<std::string> v2;
-  R->rotation(v1,v2);
+    "othellolagkage"};
+
+    for(auto &x : Solution::rotation(test))
+        cout << x.second << " → " << x.first;
 }
