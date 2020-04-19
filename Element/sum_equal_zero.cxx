12b12885f50df142124ec7860648fc456759794f
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-04-19 15:54:48 +0300

    Add files via upload

---
+++ Element/sum_equal_zero.cxx
@@ -0,0 +1,25 @@
+// Subarray which sum equal zero ~ O(N) time complexity
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+	public:
+	static bool sum_zero(vector<int> &arr){
+		for (auto i = 0; i < arr.size(); i++){
+			int sum = 0;
+			for(auto j = i; j < arr.size(); j++){
+				sum += arr[j];
+				if(sum == 0)
+					return true;
+			}
+		}
+		return false;
+	}
+};
+int main(){
+	ios_base::sync_with_stdio(0);
+	vector<int> test = {4, 2, -3, 1, 6}; // True
+	cout << Solution::sum_zero(test);
+}
