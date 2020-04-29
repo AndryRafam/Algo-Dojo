5b4a9a06a3964c4bad22dc30ed80be87371e6ef2
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-04-29 15:18:32 +0300

    Update Pair_Sum2.py

--- Pairs/Pair_Sum2.py
+++ Pairs/Pair_Sum2.py
@@ -1,9 +1,9 @@
 # Given two unsorted arrays, find all pairs whose sum is equal to a given number
-# O(max(N,M))
+# O(max(N,M)) complexity
 
 class Solution(object):
     def find_pairs(self,arr1:list, arr2:list, k:int):
-        self.set = {}
+        self.set = []
         self.set = set()
         for i in range(len(arr1)):
             self.set.add(arr1[i])
@@ -17,4 +17,4 @@ if __name__=='__main__':
     arr2 = [0,7,4,3,2,1]
     k = 8
     s = Solution()
-    s.find_pairs(arr1,arr2,k) # (8,0)(1,7)(4,4)(5,3)(7,1)
+    s.find_pairs(arr1,arr2,k) # (8,0) (1,7) (4,4) (5,3) (7,1)
