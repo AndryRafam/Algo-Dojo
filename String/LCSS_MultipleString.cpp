69e7a741bd17a844c585e75c0557c7640789344f
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-05-02 14:29:16 +0300

    Update LCSS_MultipleString.cpp

--- String/LCSS_MultipleString.cpp
+++ String/LCSS_MultipleString.cpp
@@ -1,32 +1,35 @@
-/* Program which returns the Longest Common Substring of the multiple string */
+// Find the longest common substring of multiple string
+
 
 #include <bits/stdc++.h>
 using namespace std;
 
-bool compare(string &s1, string &s2){
-	return s1.size() < s2.size();
+bool compare(string &a,string &b){
+    return a.size() < b.size();
 }
-string findStem(vector<string> arr){
-	vector<string> res;
-	string s = "";
-	for(auto i = 0; i < arr[0].length(); ++i){
-		for(auto j = 1; j <= arr[0].length()-i; ++j){
-			s = arr[0].substr(i,j);
-			auto k = 1;
-			while(k < arr.size()){
-				if(arr[k].find(s)==string::npos)
-					break;
-				k++;
-			}
-			if(k==arr.size())
-				res.emplace_back(s);
-		}
-	}
-	if(res.empty())
-		 return "KO";
-	else
-		return *max_element(res.begin(),res.end(),compare);
+
+string findStem(vector<string> &arr){
+     vector<string> res;
+     string s = "";
+     for(auto i = 0; i < arr[0].length(); ++i){
+        for(auto j = 1; j <= arr[0].length()-i; ++j){
+            s = arr[0].substr(i,j);
+            auto k = 1;
+            while(k < arr.size()){
+                if(arr[k].find(s)==string::npos)
+                    break;
+                k++;
+            }
+            if(k==arr.size()) // if s is common to every string, push it in the array
+                res.emplace_back(s);
+        }
+     }
+     if(res.empty())
+        return "KO";
+     else
+        return *max_element(res.begin(),res.end(),compare);
 }
+
 int main(){
 	ios_base::sync_with_stdio(0);
 	vector<string> test1 = {"gracefully","graceful","disgrace","disgraceful"};
