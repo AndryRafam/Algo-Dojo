30b7459a00ccec769f6500174cccea21e785a2df
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-05-02 20:28:21 +0300

    Update Longest_Substring_NoRepeated.cpp

--- String/Longest_Substring_NoRepeated.cpp
+++ String/Longest_Substring_NoRepeated.cpp
@@ -13,7 +13,7 @@ class Solution{
         for(auto i = 0; i < s.length(); ++i)
             hash[s[i]]++;
         for(auto &x : hash)
-            if(x.second != 1 ? false:true);
+            return (x.second != 1 ? false:true);
     }
     static void longestSubstring(string &s){
         string temp = "";
