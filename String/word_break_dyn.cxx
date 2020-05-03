6c4988439b1108c5ce114b8928d3927ae4b66a57
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-05-04 02:25:11 +0300

    Update word_break_dyn.cxx

--- String/word_break_dyn.cxx
+++ String/word_break_dyn.cxx
@@ -5,21 +5,21 @@ using namespace std;
 
 class Solution{
     public:
-    static bool word_break(string s, vector<string> &dict, vector<int> dp){
+    static bool word_break(string s, vector<string> &dict, vector<int> &dp){
         int n = s.size();
         if(n==0)
             return true;
         if(dp[n]==-1){
-            dp[n]=0;
+            dp[n]=false;
             for(auto i = 1; i <= n; i++){
-                string pref = s.substr(0,i);
-                if(find(dict.begin(),dict.end(),pref) != dict.end() && word_break(s.substr(i),dict,dp))
-                    return dp[n]=1;
+                if(find(dict.begin(),dict.end(),s.substr(0,i)) != dict.end() && word_break(s.substr(i),dict,dp))
+                    dp[n]=true;
             }
         }
         return dp[n];
     }
 };
+
 int main(){
     ios_base::sync_with_stdio(0);
     cout.tie(0);
