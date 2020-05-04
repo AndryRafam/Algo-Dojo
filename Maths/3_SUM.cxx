0cc6f09ae100df20ab4c38d27c90469b10a5b77d
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-05-04 21:50:57 +0300

    Update 3_SUM.cxx

--- Maths/3_SUM.cxx
+++ Maths/3_SUM.cxx
@@ -12,7 +12,6 @@ class Solution{
 	public:
 	static void three_sum(vector<int> &arr, int x){
 		int n = arr.size();
-		int flag = 0; // use to check if we have reached the end of the array
 		for(auto i = 0; i < n; i++){
 			int l = i+1;
 			int r = n-1;
@@ -26,10 +25,8 @@ class Solution{
 				else
 					r--;
 			}
-			flag+=1;
 		}
-		if(flag==n) // If we have reached the end of the array and no triplets was found
-			cout << "NO TRIPLETS";
+		cout << "NO TRIPLETS";
 	}
 };
 
@@ -38,5 +35,5 @@ int main(){
 	cout.tie(0);
 	vector<int> test = {10,20,35,50,78,80};
 	int target = 148;
-	Solution::three_sum(test,target);
+	Solution::three_sum(test,target); // 20 50 78
 }
