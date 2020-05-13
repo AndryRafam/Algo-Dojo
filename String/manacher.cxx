6e1818093c341916a394e95c7279948593861c5a
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-05-13 21:53:35 +0300

    Update manacher.cxx

--- String/manacher.cxx
+++ String/manacher.cxx
@@ -14,6 +14,8 @@ void manacher(string s){
     int n = t.size();
     int P[n];
     int c = 1, r = 1;
+    int maxLen = 0;
+    int centerIndex = 0;
     for(auto i = 2; i < n-1; i++){
         int mirror = 2*c-i;
         P[i] = max(0,min(r-i,P[mirror]));
@@ -23,10 +25,6 @@ void manacher(string s){
             c = i;
             r = i+P[i];
         }
-    }
-    int maxLen = 0;
-    int centerIndex = 0;
-    for(auto i = 2; i < n-1; i++){
         if(P[i] > maxLen){
             maxLen = P[i];
             centerIndex = i;
