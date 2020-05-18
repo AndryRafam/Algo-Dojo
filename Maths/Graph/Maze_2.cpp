c42d255126e38b98ded885f87d582beb724b5e40
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-05-18 21:04:05 +0300

    Update Maze_2.cpp

--- Maths/Graph/Maze_2.cpp
+++ Maths/Graph/Maze_2.cpp
@@ -34,11 +34,11 @@ int main(){
 	ios_base::sync_with_stdio(0);
 	cout.tie(0);
 	vector<vector<int>> maze = {{1, 0, 0, 0, 0, 0},
-								{1, 1, 0, 1, 1, 1},
-								{0, 1, 1, 1, 0, 1},
-								{0, 0, 1, 0, 1, 1},
-								{0, 1, 0, 1, 0, 1},
-								{0, 1, 1, 1, 0, 1}}; // True in this example
+				    {1, 1, 0, 1, 1, 1},
+				    {0, 1, 1, 1, 0, 1},
+				    {0, 0, 1, 0, 1, 1},
+				    {0, 1, 0, 1, 0, 1},
+				    {0, 1, 1, 1, 0, 1}}; // True in this example
 								
 	cout << (Solution::path(maze)==1 ? "True":"False") << endl;
 	return 0;
