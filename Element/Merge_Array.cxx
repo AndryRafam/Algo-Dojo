f29a6ffdfd8eb5af6b7e18f652886145e4163c19
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-10-01 00:39:34 +0300

    Update Merge_Array.cxx

--- Element/Merge_Array.cxx
+++ Element/Merge_Array.cxx
@@ -5,33 +5,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> merge(vector<int> a, vector<int> b){
-    vector<int> res;
+class Solution {
+    public:
+    static vector<int> merge(vector<int> a, vector<int> b){
+        vector<int> res;
 
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
 
-    int i = 0, j = 0;
-    while(i < (int)a.size() && j < (int)b.size()){
-        if(a[i] <= b[j]){
+        int i = 0, j = 0;
+        while(i < (int)a.size() && j < (int)b.size()){
+            if(a[i] <= b[j]){
+                res.emplace_back(a[i]);
+                i++;
+            }
+            else{
+                res.emplace_back(b[j]);
+                j++;
+            }
+        }
+        while(i < (int)a.size()){
             res.emplace_back(a[i]);
             i++;
         }
-        else{
+        while(j < (int)b.size()){
             res.emplace_back(b[j]);
             j++;
         }
+        return res;
     }
-    while(i < (int)a.size()){
-        res.emplace_back(a[i]);
-        i++;
-    }
-    while(j < (int)b.size()){
-        res.emplace_back(b[j]);
-        j++;
-    }
-    return res;
-}
+};
 
 int main(){
     ios_base::sync_with_stdio(0);
@@ -39,7 +42,7 @@ int main(){
     vector<int> a = {10,5,15};
     vector<int> b = {20,3,2,12};
     
-    for(auto &x : merge(a,b))
+    for(auto &x : Solution::merge(a,b))
         cout << x << " ";
     cout << endl;
     return 0;
