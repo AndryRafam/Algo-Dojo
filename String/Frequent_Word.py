999404c43e948969459dfbd72e9a86e5e7dedadb
Andry Rafam Andrianjafy <andryrafam@protonmail.com>
2020-10-10 16:43:05 +0300

    Add files via upload

---
+++ String/Frequent_Word.py
@@ -0,0 +1,18 @@
+# Most frequent word in array.
+
+from collections import Counter
+
+def freq(vec):
+	hash = Counter(vec)
+	max = 0
+	s = ""	
+	for (key,val) in hash.items():
+		if(max < val):
+			max = val
+			s = key
+	print(s,end="\n")
+	
+if __name__=='__main__':
+		vec = ["jones","paul","jones","peter","jones","peter","jackson","peter"]
+		freq(vec)
+			
