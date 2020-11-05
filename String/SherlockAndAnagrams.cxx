b6f93f231b9b446f81b96a25c7c103fa134ca493
Andry Rafam Andrianjafy <andryrafam@protonmail.com>
2020-11-05 18:44:41 +0300

    Add files via upload

---
+++ String/SherlockAndAnagrams.cxx
@@ -0,0 +1,40 @@
+// Sherlock and Anagrams
+// Given a string, find the number of pairs of substrings of the string 
+// that are anagrams of each other.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool is_anagram(string a, string b){
+    auto count = 0;
+    if(a.size()!=b.size())
+        return false;
+    for(auto x : a)
+        count+=x;
+    for(auto y : b)
+        count-=y;
+    return (count==0 ? true:false);
+}
+
+int SherlockAndAnagrams(string s){
+    auto count = 0;
+    vector<string> r;
+    for(auto i(0); i < (int)s.length(); i++){
+        for(auto j(1); j <= (int)s.length()-i; j++){
+            r.emplace_back(s.substr(i,j));
+        }
+    }
+    for(auto i(0); i < (int)r.size(); i++){
+        for(auto j(i+1); j < (int)r.size(); j++){
+            if(is_anagram(r[i],r[j]))
+                count++;
+        }
+    }
+    return count;
+}
+
+int main(){
+    string s = "kkkk";
+    printf("%d\n",SherlockAndAnagrams(s));
+    return 0;
+}
