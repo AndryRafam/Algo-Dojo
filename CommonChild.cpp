9fe69db492b83f84925ed94b98cc1328577fb2d7
Andry Rafam Andrianjafy <andryrafam@protonmail.com>
2021-04-25 14:54:39 +0300

    Create CommonChild.cpp

---
+++ CommonChild.cpp
@@ -0,0 +1,29 @@
+// Common Child problem from Hackerrank
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int commonChild(string s1, string s2){
+	int m = s1.length();
+	int n = s2.length();
+	int dp[m+1][n+1];
+	for(auto i = 0; i <= m; i++){
+		for(auto j = 0; j <= n; j++){
+			if(i==0 || j==0)
+				dp[i][j] = 0;
+			else if(s1[i-1]==s2[j-1])
+				dp[i][j] = dp[i-1][j-1]+1;
+			else
+				dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+		}
+	}
+	return dp[m][n];
+}
+
+int main()
+{
+	string s1 = "SHINCHAN";
+	string s2 = "NOHARAAA";
+	cout << commonChild(s1,s2) << endl; // 3
+	return 0;
+}
