bd8607874c3be842f97add3901efbd391ec06134
Andry Rafam <andryrafam@protonmail.com>
2023-02-28 17:11:10 +0300

    Update SherlockAndAnagrams.cxx

--- String/SherlockAndAnagrams.cxx
+++ String/SherlockAndAnagrams.cxx
@@ -1,42 +1,48 @@
 // Sherlock and Anagrams
-// Given a string, find the number of pairs of substrings of the string 
+// Given a string, find the number of pairs of substrings of the string
 // that are anagrams of each other.
 
 #include <bits/stdc++.h>
 using namespace std;
 
-bool is_anagram(string a, string b){
-    auto count = 0;
-    if(a.size()!=b.size())
-        return false;
-    for(auto x : a)
-        count+=x;
-    for(auto y : b)
-        count-=y;
-    return (count==0 ? true:false);
-}
-
-int SherlockAndAnagrams(string s){
-    auto count = 0;
-    vector<string> r;
-    for(auto i(0); s[i]; i++){
-        for(auto j(1); j <= (int)s.length()-i; j++){
-            r.emplace_back(s.substr(i,j));
+class Solution{
+    public:
+    static bool is_anagram(string a, string b){
+        unordered_map<char,int> h;
+        if(a.size()!=b.size())
+            return false;
+        for(auto x(0); a[x]&&b[x]; x++){
+            h[a[x]]++;
+            h[b[x]]--;
         }
-    }
-    for(auto i(0); i < (int)r.size(); i++){
-        for(auto j(i+1); j < (int)r.size(); j++){
-            if(is_anagram(r[i],r[j]))
-                count++;
+        for(auto &y : h){
+            if(y.second!=0)
+                return false;
         }
+        return true;
     }
-    return count;
-}
+    static int SherlockAndAnagrams(string s){
+        auto count = 0;
+        vector<string> r;
+        for(auto i(0); s[i]; i++){
+            for(auto j(1); j <= (int)s.length()-i; j++){
+                r.emplace_back(s.substr(i,j));
+            }
+        }
+        for(auto i(0); i < (int)r.size(); i++){
+            for(auto j(i+1); j < (int)r.size(); j++){
+                if(is_anagram(r[i],r[j]))
+                    count++;
+            }
+        }
+        return count;
+    }
+};
 
 int main(){
     string a = "kkkk";
     string b = "ifailuhkqq";
-    printf("%d\n",SherlockAndAnagrams(a)); // 10
-    printf("%d\n",SherlockAndAnagrams(b)); // 3
+    cout << Solution::SherlockAndAnagrams(a) << endl; // 10
+    cout << Solution::SherlockAndAnagrams(b) << endl; // 3
     return 0;
 }
