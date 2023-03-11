eb828e7e0a8de7b2cbe40273a600cd11ba788b66
Andry Rafam <andryrafam@protonmail.com>
2023-03-12 00:48:26 +0300

    Add files via upload

---
+++ Maths/Smallest_K.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> smallest_k(vector<int> arr, int k) {
+    priority_queue<int> maxHeap;
+    
+    for(auto i(0); i < k; ++i) {
+        maxHeap.push(arr[i]);
+    }
+    for(auto i(k); i < arr.size(); ++i) {
+        if(arr[i] < maxHeap.top()) {
+            maxHeap.pop();
+            maxHeap.push(arr[i]);
+        }
+    }
+    vector<int> result;
+    while(!maxHeap.empty()) {
+        result.emplace_back(maxHeap.top());
+        maxHeap.pop();
+    }
+    sort(result.begin(),result.end());
+    return result;
+}
+
+int main() {
+    int k;
+    cin >> k;
+    vector<int> arr = {25, 7, 20, 15, 9, 36, 50};
+    for(auto x : smallest_k(arr,k)) {
+        cout << x << " ";
+    }
+    return 0;
+}
