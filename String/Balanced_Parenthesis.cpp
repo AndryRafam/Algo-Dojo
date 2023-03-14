404641c3ec5b3168a780f859f3e538e04b1bf5ef
Andry Rafam <andryrafam@protonmail.com>
2023-03-14 18:23:07 +0300

    Update Balanced_Parenthesis.cpp

--- String/Balanced_Parenthesis.cpp
+++ String/Balanced_Parenthesis.cpp
@@ -18,30 +18,18 @@ class Solution{
 		for(auto x : str){
 			if(x =='(' || x == '{' || x =='['){
 				s.push(x);
-				continue;
 			}
-			if(s.empty())
-				return true;
-			switch (x){
-				case ')':
-					ch = s.top();
-					s.pop();
-					if(ch == '{' || ch == '[')
-						return false;
-						break;
-				case ']':
-					ch = s.top();
-					s.pop();
-					if(ch == '(' || ch == '{')
-						return false;
-						break;
-				case '}':
-					ch = s.top();
-					s.pop();
-					if(ch == '(' || ch == '[')
-						return false;
-						break;
+			else{
+				if(s.empty()){
+					return true;
+				}
+				char top = s.top();
+				s.pop();
+				if((top=='(' && x!=')') || (top=='{' && x!='}') || (top=='[' && x!=']')){
+					return false;
+				}
 			}
+			
 		}
 		return (s.empty());
 	}
@@ -51,8 +39,10 @@ int main(){
 	ios_base::sync_with_stdio(false);
 	string test1 = "()[]{}";
 	string test2 = "([)]";
-	string test3 = " ";
-	cout << Solution::balanced(test1) << endl;
-	cout << Solution::balanced(test2) << endl;
-	cout << Solution::balanced(test3);
+	string test3 = "{()}[]";
+	string test4 = ""; // empty string
+	cout << Solution::balanced(test1) << endl; // 1
+	cout << Solution::balanced(test2) << endl; // 0
+	cout << Solution::balanced(test3) << endl; // 1
+	cout << Solution::balanced(test4); // 1
 }
