6bc7cabc9c0e163258f4f8207df4482279168425
Andry Rafam <andryrafam@protonmail.com>
2023-03-21 20:19:05 +0300

    Update Top_K_Frequent_Elements.cpp

--- Element/Top_K_Frequent_Elements.cpp
+++ Element/Top_K_Frequent_Elements.cpp
@@ -1,24 +1,37 @@
-/* Top K_frequent_elements */
+/*
+Given an integer array nums and an integer k, return the k most frequent elements. 
+You may return the answer in any order.
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution{
     public:
-        static void k_elements(vector<int> &nums, int k){
-            unordered_map<int,int> hash;
-            for(auto i = 0; i < nums.size(); i++)
-                hash[nums[i]]++;
-            for(auto &x : hash){
-                if(x.second >= k)
-                    cout << x.first << " ";
-            }
+    static vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> ans;
+        unordered_map<int,int> freq;
+        for(auto i : nums) {
+            freq[i]++;
         }
+        priority_queue<pair<int,int>> maxHeap;
+        for(auto j : freq) {
+            maxHeap.push({j.second,j.first});
+        }
+        while(k--) {
+            ans.emplace_back(maxHeap.top().second);
+            maxHeap.pop();
+        }
+        return ans;
+    }
 };
 
 int main(){
     ios_base::sync_with_stdio(false);
     int k; cin >> k;
-    vector<int> tab = {1,1,2,1,2,3};
-    Solution::k_elements(tab,k);
+    vector<int> tab = {1,1,1,2,2,3};
+    for(auto x : Solution::topKFrequent(tab,k)) { // [1 2], for k = 2
+        cout << x << " ";
+    }
+    return 0;
 }
