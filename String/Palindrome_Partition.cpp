5b9af859249ed3b7bf5a4011fc79f5e0aabc2df2
Andry Rafam <andryrafam@protonmail.com>
2023-03-24 14:31:24 +0300

    Update Palindrome_Partition.cpp

--- String/Palindrome_Partition.cpp
+++ String/Palindrome_Partition.cpp
@@ -8,16 +8,17 @@ Time complexity: O(N*2^N), where N is the length of the string S.
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<vector<string>> result;
+vector<string> currentList;
+
 class Solution {
     public:
     static vector<vector<string>> partition(string s) {
-        vector<vector<string>> result;
-        vector<string> currentList;
-        dfs(result,s,0,currentList);
+        dfs(0,s);
         return result;
     }
     private:
-    static void dfs(vector<vector<string>> &result, string &s, int start, vector<string> &currentList) {
+    static void dfs(int start, string &s) {
         if(start >= s.length()) {
             result.emplace_back(currentList);
         }
@@ -26,7 +27,7 @@ class Solution {
                 // add current palindrome substring in the current list
                 currentList.emplace_back(s.substr(start,end-start+1));
                 // backtrack and remove the current palindrome substring from currentList
-                dfs(result,s,end+1,currentList);
+                dfs(end+1,s);
                 currentList.pop_back();
             }
         }
