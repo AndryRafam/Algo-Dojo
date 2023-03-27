dd42c2e5530a604e43f3797efb0070591e5d81c5
Andry Rafam <andryrafam@protonmail.com>
2023-03-27 19:26:15 +0300

    Update 3_SUM.cxx

--- Maths/3_SUM.cxx
+++ Maths/3_SUM.cxx
@@ -1,39 +1,52 @@
-/* 3 SUM problem
- * 
- * Complexity: O(N²) if the array is already sorted
- *             O(N²+NlogN) if the array is not sorted (we must sort the array first)*/
- 
-// In this example, the array is already sorted (O(N²)) complexity
- 
+/*
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] 
+such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Time complexity: O(N²logN), sort the array first and then apply two pointers approach.
+				 N is the size of the array.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
+class Solution {
 	public:
-	static void three_sum(vector<int> &arr, int x){
-		int n = arr.size();
-		for(auto i = 0; i < n; i++){
-			int l = i+1;
-			int r = n-1;
-			while(l < r){
-				if(arr[i]+arr[l]+arr[r]==x){
-					cout << arr[i] << " " << arr[l] << " " << arr[r];
-					exit(0);
+	static vector<vector<int>> threeSum(vector<int> &nums) {
+		set<vector<int>> tmp;
+		sort(nums.begin(),nums.end());
+		for(int i(0); i < nums.size(); ++i) {
+			int j = i+1;
+			int k = nums.size()-1;
+			while(j < k) {
+				if(nums[i]+nums[j]+nums[k]==0) {
+					tmp.insert({nums[i],nums[j],nums[k]});
+					j++;
+					k--;
 				}
-				else if(arr[i]+arr[l]+arr[r] < x)
-					l++;
-				else
-					r--;
+				else if(nums[i]+nums[j]+nums[k] < 0) {
+					j++;
+				}
+				else {
+					k--;
+				}
 			}
 		}
-		cout << "NO TRIPLETS";
+		vector<vector<int>> result;
+		for(auto &x : tmp) {
+			result.emplace_back(x);
+		}
+		return result;
 	}
 };
 
 int main(){
 	ios_base::sync_with_stdio(0);
-	cout.tie(0);
-	vector<int> test = {10,20,35,50,78,80};
-	int target = 148;
-	Solution::three_sum(test,target); // 20 50 78
+	vector<int> test = {-1,0,1,2,-1,-4};
+	for(auto y : Solution::threeSum(test)) {
+		for(auto x : y) {
+			cout << x << " ";
+		}
+		cout << "\n";
+	}
+	return 0;
 }
