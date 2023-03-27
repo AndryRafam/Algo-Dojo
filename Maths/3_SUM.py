cf0cc150970d6e942611b1fa29940b639f4b60ae
Andry Rafam <andryrafam@protonmail.com>
2023-03-27 20:03:16 +0300

    Add files via upload

---
+++ Maths/3_SUM.py
@@ -0,0 +1,29 @@
+"""
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] 
+such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Time complexity: O(N²logN), sort the array first and then apply two pointers approach.
+				 N is the size of the array.
+"""
+
+def threeSum(nums):
+    nums.sort()
+    result = set()
+    for i in range(len(nums)):
+        j = i+1
+        k = len(nums)-1
+        while (j < k):
+            sum = nums[i]+nums[j]+nums[k]
+            if sum==0:
+                result.add((nums[i],nums[j],nums[k]))
+                k-=1
+                j+=1
+            elif sum < 0:
+                j+=1
+            else:
+                k-=1
+    return result
+
+if __name__=="__main__":
+    test = [-1,0,1,2,-1,-4]
+    print(threeSum(test),end=" ")
