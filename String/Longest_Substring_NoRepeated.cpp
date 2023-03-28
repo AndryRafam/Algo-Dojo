1ee9e8954c10d0c7881ceff1939eea7055ec9df6
Andry Rafam <andryrafam@protonmail.com>
2023-03-28 11:15:14 +0300

    Update Longest_Substring_NoRepeated.cpp

--- String/Longest_Substring_NoRepeated.cpp
+++ String/Longest_Substring_NoRepeated.cpp
@@ -1,39 +1,44 @@
-// Longest Substring without repeated character
+/*
+Given a string s, find the length of the longest substring without repeating characters.
+
+Time complexity: O(N³), where N is the length of the string.
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
-    public:
-    static bool compare(string &s1, string &s2){ // compare the size of the string
-        return s1.size() < s2.size();
-    }
-    static bool unique(string &s){ // check if there is a repeated character
-        unordered_map<char,int> hash;
-        for(auto i = 0; i < s.length(); ++i)
-            hash[s[i]]++;
-        for(auto &x : hash)
-            if (x.second != 1)
-                return false;
-        return true;
-    }
-    static void longestSubstring(string &s){
-        string temp = "";
-        vector<string> str;
-        for(auto i = 0; i < s.length(); ++i){
-            for(auto j = 1; j <= s.length()-i; ++j){
-                temp = s.substr(i,j);
-                if(unique(temp))
-                    str.emplace_back(temp);
-            }
-        }
-        sort(str.begin(),str.end(),compare);
-        cout << str[str.size()-1] << " -> " << str[str.size()-1].length();
-    }
+class Solution {
+	public:
+	static int lengthOfLongestSubstring(string &s) {
+		int max = 0;
+		int l = 1;
+		while(l < s.length()) {
+			for(int i(0); i <= s.length()-l; ++i) {
+				string temp = s.substr(i,l);
+				if(isUnique(temp)) {
+					max = temp.length();
+				}
+			}
+			l++;
+		}
+		return max;
+	}
+	private:
+	static bool isUnique(string &a) {
+		unordered_map<char,int> hash;
+		for(auto &x : a) {
+			hash[x]++;
+		}
+		if(hash.size()==a.length()) {
+			return true;
+		}
+		return false;
+	}
 };
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    string test = "pwwkeouwke";
-    Solution::longestSubstring(test);
+int main() {
+	ios_base::sync_with_stdio(false);
+	string s = "abcabcbb";
+	cout << Solution::lengthOfLongestSubstring(s);
+	return 0;
 }
