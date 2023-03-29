ddd04fa17f1d4a085dcc381b526f3d560a56b22e
Andry Rafam <andryrafam@protonmail.com>
2023-03-29 13:40:08 +0300

    Create Escape_Maze.cpp

---
+++ Maths/Escape_Maze.cpp
@@ -0,0 +1,66 @@
+/*
+Can you exit the maze ?
+Maze problem: Starting from top left corner, find a way to the bottom right corner.
+All directions is allowed Up, Down, Right, Left.
+*/ 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+	public:
+	static bool dfs(vector<vector<int>> &maze, int start_x, int start_y){
+		int N = maze.size();
+		int M = maze[0].size();
+		vector<int> row = {-1,0,0,1};
+		vector<int> col = {0,-1,1,0};
+		if(start_x<0 || start_x>=N || start_y<0 || start_y>=M || maze[start_x][start_y]!=1){
+			return false;
+		}
+		/*if(maze[start_x][start_y]==1){
+			maze[start_x][start_y]='$'; // mark cell as visited, so we can't visit it anymore
+			if(start_x==N-1 && start_y==M-1){
+				return true;
+			}
+			for(auto k = 0; k < 4; k++){
+				if(dfs(maze,start_x+row[k],start_y+col[k])){
+					return true;
+				}
+			}
+			
+		}*/
+		if(start_x==N-1 && start_y==M-1){
+			return true;
+		}
+		else if(maze[start_x][start_y]==1){
+			maze[start_x][start_y]='$';
+			for(int k(0); k < 4; ++k){
+				if(dfs(maze,start_x+row[k],start_y+col[k])){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	static bool path(vector<vector<int>> &maze){
+		if(!dfs(maze,0,0)){
+			return false;
+		}
+		return true;
+	}
+};
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cout.tie(0);
+	vector<vector<int>> maze = {{1, 0, 0, 1, 1, 1},
+								{1, 1, 0, 1, 0, 1},
+								{0, 1, 1, 1, 0, 1},
+								{0, 0, 1, 0, 1, 1},
+								{0, 0, 1, 0, 1, 0},
+								{0, 0, 1, 0, 1, 1}}; // True in this example
+								
+	cout << (Solution::path(maze)==1 ? "True":"False") << endl;
+	return 0;
+}
