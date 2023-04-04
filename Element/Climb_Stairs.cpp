c6f41a2ece71b0699d1d6087505d4a94d6e1fb04
Andry Rafam <andryrafam@protonmail.com>
2023-04-04 11:34:32 +0300

    Update Climb_Stairs.cpp

--- Element/Climb_Stairs.cpp
+++ Element/Climb_Stairs.cpp
@@ -1,6 +1,7 @@
 /* You are climbing a staircase. It takes n steps to reach the top.
  * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
- * Time complexity && O(N) space complexity.
+ * Time complexity: O(N)
+ * Space complexity: O(N)
  */
 
 #include <bits/stdc++.h>
