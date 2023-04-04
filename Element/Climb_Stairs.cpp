70d6b50bb47a25dacd6a57666381aa6d0db4374c
Andry Rafam <andryrafam@protonmail.com>
2023-04-04 11:24:20 +0300

    Update Climb_Stairs.cpp

--- Element/Climb_Stairs.cpp
+++ Element/Climb_Stairs.cpp
@@ -1,4 +1,7 @@
-/* Climb stairs - O(N) time complexity && O(N) space complexity*/
+/* You are climbing a staircase. It takes n steps to reach the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ * Time complexity && O(N) space complexity.
+ */
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -8,16 +11,16 @@ class Solution{
     static int climbStairs(int n){
         if(n==1)
             return 1;
-        int dp[n+1];
-        dp[1]=1;
-        dp[2]=2;
-        for(auto i = 3; i <= n; ++i)
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        for(auto i = 1; i <= n; ++i)
             dp[i]=dp[i-1]+dp[i-2];
         return dp[n];
     }
 };
+
 int main(){
     ios_base::sync_with_stdio(false);
-    int n = 4;
-    cout << Solution::climbStairs(n);
+    int n = 12;
+    cout << Solution::climbStairs(n); // 233
 }
