9e88417c5e8f443eb86ba1ad288b29a3eec19bda
Andry Rafam <andryrafam@protonmail.com>
2023-04-05 10:56:53 +0300

    Update 2_SUM.cxx

--- Maths/2_SUM.cxx
+++ Maths/2_SUM.cxx
@@ -1,4 +1,5 @@
-/* Given an array of size N and a number "c", determine whether or not there exist pairs of numbers "a" and "b", such that a+b=c
+/* Given an array of size N and a number "c", determine whether or not there exist pairs of numbers "a" and "b", 
+such that a+b=c. If there is an answer, print their indices.
 
 Time Complexity: O(N)
 Space Complexity: O(N)*/
@@ -8,23 +9,26 @@ using namespace std;
 
 class Solution{
 	public:
-	static bool pair_exist(vector<int> &arr, int c){
+	static vector<int> pairExist(vector<int> &arr, int c){
 		unordered_map<int,int> hashmap;
+		vector<int> ans;
 		for(auto i(0); i < arr.size(); ++i){
 			int cmp = c-arr[i];
 			if(hashmap.find(cmp)!=hashmap.end()){
-				return true;
+				ans.emplace_back(hashmap[cmp]);
+				ans.emplace_back(i);
+				return ans;
 			}
 			hashmap[arr[i]]=i;
 		}
-		return false;
+		return ans;
 	}
 };
 
 int main(int argc, char **argv){
 	vector<int> arr = {10,50,20,35,80,78};
-	string res;
-	res = (Solution::pair_exist(arr,30)==true) ? "YES":"NO";
-	cout << res;
+	for(auto &x : Solution::pairExist(arr,30)){
+		cout << x << " ";
+	}
 	return 0;
 }
