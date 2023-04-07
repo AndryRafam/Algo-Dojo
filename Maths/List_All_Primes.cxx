82d9751b543f5c20770f3792c433e14f5663e840
Andry Rafam <andryrafam@protonmail.com>
2023-04-07 14:48:22 +0300

    Add files via upload

---
+++ Maths/List_All_Primes.cxx
@@ -0,0 +1,45 @@
+/*
+Write a program that takes an integer argument and returns all prime numbers between 1 and the integer (not included).
+
+Brute force approach
+
+Time complexity: O(N*sqrt(N)).
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+	private:
+	static bool isPrime(int n){
+		if(n== 0 || n==1){
+			return false;
+		}
+		for(auto i(2); i <= sqrt(n); ++i){
+			if(n%i==0){
+				return false;
+			}
+		}
+		return true;
+	}
+	public:
+	static vector<int> listAllPrimes(int n){
+		vector<int> primes;
+		for(auto x(0); x < n; ++x){ // exclude n even if it is prime number
+			if(isPrime(x)){
+				primes.emplace_back(x);
+			}
+		}
+		return primes;
+	}
+};
+// driver program
+int main(){
+	ios_base::sync_with_stdio(false);
+	int n;
+	cin >> n;
+	for(auto &k : Solution::listAllPrimes(n)){
+		cout << k << " ";
+	}
+	return 0;
+}
---
+++ Maths/List_All_Primes_2.cxx
@@ -0,0 +1,40 @@
+/*
+ * Write a program that takes an integer argument and returns all prime numbers between 1 and the integer (not included).
+ * 
+ * Sieve of eratosthenes approach.
+ * 
+ * Time complexity: O(Nlog(logN))
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+	public:
+	static vector<int> listAllPrimes(int n){
+		vector<int> primes;
+		vector<bool> is_prime(n+1,true);
+		is_prime[0] == is_prime[1] == false;
+		
+		for(auto p(2); p < n; p++){ // exclude n even if it is a prime number
+			if(is_prime[p]){
+				primes.emplace_back(p);
+			}
+			// sieve p multiples
+			for(auto j(p); j <=n; j+=p){ 
+				is_prime[j] = false;
+			}
+		}
+		return primes;
+	}
+};
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	int n;
+	cin >> n;
+	for(auto &k : Solution::listAllPrimes(n)){
+		cout << k << " ";
+	}
+	return 0;
+}
