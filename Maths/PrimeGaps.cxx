945070ef9ab4589b873cd9d821d954ebd203c7b8
Andry Rafam <andryrafam@protonmail.com>
2023-04-08 16:04:07 +0300

    Update PrimeGaps.cxx

--- Maths/PrimeGaps.cxx
+++ Maths/PrimeGaps.cxx
@@ -1,38 +1,48 @@
-// Source: https://edabit.com/challenge/ShunivMZuPRgEg7iQ
+/*
+ * Given an integer g >= 2, an integer m > 2 and an integer n >= m;
+ * return the first pair of prime numbers between m and n, that has a gap of size g.
+ * If there is no such prime numbers, return null.
+ *
+ * Time complexity: O((N-M)²)*O(Ksqrt(K)) = O((N-M)²Ksqrt(K))
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
 
-bool prime(int n){
-    if(n < 2)
-        return false;
-    for (auto x = 2; x*x <= n; x++){
-        if(n%x==0)
-            return false;
-    }
-    return true;
-}
-
-void prime_gaps(int g, int a, int b){
-    vector<int> t;
-    for(auto i = a; i <= b; i++){
-        if(prime(i))
-            t.emplace_back(i);
-    }
-    for(auto x = 0; x < (int)t.size(); x++){
-        for(auto y = x+1; y < (int)t.size(); y++){
-            if(t[y]-t[x]==g){
-                cout << t[x] << " " << t[y] << endl;
-                return;
-            }
-        }
-    }
-    cout << 0 << " " << 0 << endl;
-    return;
-}
+class Solution{
+	private:
+	static bool isPrime(int x){
+		if(x==0 || x==1){
+			return false;
+		}
+		for(auto p(2); p <= sqrt(x); ++p){
+			if(x%p==0){
+				return false;
+			}
+		}
+		return true;
+	}
+	public:
+	static vector<pair<int,int>> gap (int g, int m, int n){
+		vector<pair<int,int>> res;
+		for(auto i(m); i < n; ++i){
+			for(auto j(i+1); j < n; ++j){
+				if(isPrime(i) && isPrime(j) && j-i==g){
+					res.emplace_back(i,j);
+					return res;
+				}
+			}
+		}
+		return {{0,0}};
+	}
+};
 
 int main(){
-    int g = 4;
-    int a = 130; int b = 200;
-    prime_gaps(g,a,b); // 163 167
+	ios_base::sync_with_stdio(false);
+	int g,m,n;
+	cin >> g >> m >> n;
+	for(auto &x : Solution::gap(g,m,n)){
+		cout << x.first << " " << x.second << "\n";
+	}
+	return 0;
 }
