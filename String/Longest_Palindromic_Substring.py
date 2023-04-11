4d9dc926f2a29f3646393de728fa47de7e94f92c
Andry Rafam <andryrafam@protonmail.com>
2023-04-11 20:27:39 +0300

    Update Longest_Palindromic_Substring.py

--- String/Longest_Palindromic_Substring.py
+++ String/Longest_Palindromic_Substring.py
@@ -1,25 +1,31 @@
 # Longest palindromci substring - O(N³) complexity
 
-def isPalindrome(s:str)->bool:
-    l = 0
-    r = len(s)-1
-    while(l < r):
-        if s[l]!=s[r]:
-            return False
-        l+=1
-        r-=1
-    return True
-    
-def longestPalindrome(s:str)->str:
-    maxi = 0
-    for i in range(len(s)):
-        for j in range(i+1,len(s)+1):
-            temp = s[i:j]
-            if isPalindrome(temp) and maxi < len(temp):
-                maxi = len(temp)
-                longest_pal = temp
-    return longest_pal
+class Solution:
+    def longestPalindrome(self,s:str)->str:
+        maxi = 0
+        longest_pal = ""
+
+        def isPalindrome(s:str)->bool:
+            l = 0
+            r = len(s)-1
+            while l < r:
+                if s[l]!=s[r]:
+                    return False
+                l+=1
+                r-=1
+            return True
+
+        for i in range(len(s)):
+            for j in range(i+1,len(s)+1):
+                temp = s[i:j]
+                if isPalindrome(temp) and maxi < len(temp):
+                    maxi = len(temp) 
+                    longest_pal = temp 
+        
+        return longest_pal
+
 
 if __name__=="__main__":
     test = "xoxoxbaobab"
-    print(longestPalindrome(test),end=" ")
+    s = Solution()
+    print(s.longestPalindrome(test),end=" ")
