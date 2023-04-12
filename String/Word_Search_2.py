68f706a9c33696d5f21b05d39b1e58a61536cb79
Andry Rafam <andryrafam@protonmail.com>
2023-04-12 20:44:18 +0300

    Add files via upload

---
+++ String/Word_Search_2.py
@@ -0,0 +1,37 @@
+"""
+Given an m x n board of characters and a list of strings words, return all words on the board.
+Each word must be constructed from letters of sequentially adjacent cells, 
+where adjacent cells are horizontally or vertically neighboring. 
+The same letter cell may not be used more than once in a word.
+
+Brute force approach.
+"""
+
+def findWords(board,words):
+    ust = set() # to ensure that there is no duplicate in the final solution
+    result = []
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            for k in range(len(words)):
+                if board[i][j]==words[k][0] and dfs(board,words[k],i,j,0):
+                    ust.add(words[k])
+    for x in ust:
+        result.append(x)
+    return result
+
+def dfs(board,word,i,j,pos):
+    if pos==len(word):
+        return True
+    if i < 0 or i >= len(board) or j < 0 or j >= len(board[i]) or board[i][j]!=word[pos]:
+        return False
+    else:
+        temp = board[i][j]
+        board[i][j] = '$'
+        found = dfs(board,word,i+1,j,pos+1) or dfs(board,word,i-1,j,pos+1) or dfs(board,word,i,j+1,pos+1) or dfs(board,word,i,j-1,pos+1)
+        board[i][j] = temp
+        return found
+
+if __name__=="__main__":
+    board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]
+    words = ["oath","pea","eat","rain"]
+    print(findWords(board,words),end = " ")
