e06095d403d16d4a4bb202ba39dbfd3ffce12270
Andry Rafam <andryrafam@protonmail.com>
2023-04-14 00:17:05 +0300

    Update Word_Search.cpp

--- String/Word_Search.cpp
+++ String/Word_Search.cpp
@@ -9,17 +9,16 @@ using namespace std;
 
 class Solution {
 	public:
-	static bool exist(vector<vector<char>> &board, string word) {
-		// vector<string> result;
+	static vector<string> find(vector<vector<char>> &board, string word) {
+		unordered_set<string> result;
 		for(auto i(0); i!=(int)board.size(); ++i) {
 			for(auto j(0); j!=(int)board[i].size(); ++j) {
 				if(board[i][j]==word[0] && dfs(board,word,i,j,0)) {
-					return true;
-					// result.emplace_back(word);
+					result.insert(word);
 				}
 			}
 		}
-		return false;
+		return vector<string>(result.begin(),result.end());
 	}
 	static bool dfs(vector<vector<char>> &board, string word, int i, int j, int k) {
 		if(k==(int)word.size()) {
@@ -28,19 +27,29 @@ class Solution {
 		if(i < 0 || i >= (int)board.size() || j < 0 || j >= (int)board[i].size() || board[i][j]!=word[k]) {
 			return false;
 		}
-		char temp = board[i][j];
-		board[i][j] = '$'; // mark after visit
-		bool found = dfs(board,word,i+1,j,k+1) || dfs(board,word,i-1,j,k+1) || dfs(board,word,i,j+1,k+1) || dfs(board,word,i,j-1,k+1);
-		board[i][j]=temp;
-		return found;
+		else {
+            char temp = board[i][j];
+            board[i][j] = '$'; // mark as visited
+            bool found = dfs(board,word,i+1,j,k+1) || 
+						 dfs(board,word,i-1,j,k+1) || 
+						 dfs(board,word,i,j+1,k+1) || 
+						 dfs(board,word,i,j-1,k+1);
+            board[i][j]=temp;
+            return found;
+		}
 	}
 };
 
 int main(){
 	ios_base::sync_with_stdio(0);
 	cout.tie(0);
-	vector<vector<char>> board = {{'a','b','c','e'},{'s','f','c','s'},{'a','d','e','e'}};
-	string word = "cced";
-	cout << Solution::exist(board,word) << endl;
+	vector<vector<char>> board = {{'a','b','c','e'},
+								  {'s','f','c','s'},
+								  {'a','d','e','e'}};
+	string word = "escc";
+	//cout << Solution::find(board,word) << endl;
+	for(auto x : Solution::find(board,word)) {
+		cout << x << " ";
+	}
 	return 0;
 }
