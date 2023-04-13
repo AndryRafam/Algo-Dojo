a6edaa9a61c9ed4b422a1d1adf03254982aa8466
Andry Rafam <andryrafam@protonmail.com>
2023-04-13 18:33:50 +0300

    Update Word_Search_2.cpp

--- String/Word_Search_2.cpp
+++ String/Word_Search_2.cpp
@@ -3,7 +3,6 @@ Given an m x n board of characters and a list of strings words, return all words
 Each word must be constructed from letters of sequentially adjacent cells, 
 where adjacent cells are horizontally or vertically neighboring. 
 The same letter cell may not be used more than once in a word.
-
 Brute force approach.
 */
 
@@ -15,14 +14,14 @@ class Solution{
     static vector<string> findWords(vector<vector<char>> &board, vector<string> &words){
         unordered_set<string> ust; // to ensure that there is no duplicate in the final result
         for(auto &word : words){
-		for(auto i(0); i < board.size(); ++i){
-			for(auto j(0); j < board[0].size(); ++j){
-				if(board[i][j]==word[0] && dfs(board,word,i,j,0)){
-					ust.insert(word);
-				}
-			}
-		}
-	}
+            for(auto i(0); i < board.size(); ++i){
+                for(auto j(0); j < board[0].size(); ++j){
+                    if(board[i][j]==word[0] && dfs(board,word,i,j,0)){
+                        ust.insert(word);
+				    }
+			    }
+		    }
+	    }
         return vector<string>(ust.begin(),ust.end());
     }
     private:
