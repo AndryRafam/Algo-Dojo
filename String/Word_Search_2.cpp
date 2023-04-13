8c17c1717c74e961550c3338ab2a0ce4cf7f6553
Andry Rafam <andryrafam@protonmail.com>
2023-04-13 19:27:57 +0300

    Update Word_Search_2.cpp

--- String/Word_Search_2.cpp
+++ String/Word_Search_2.cpp
@@ -3,7 +3,8 @@ Given an m x n board of characters and a list of strings words, return all words
 Each word must be constructed from letters of sequentially adjacent cells, 
 where adjacent cells are horizontally or vertically neighboring. 
 The same letter cell may not be used more than once in a word.
-Brute force approach.
+
+Brute force approach: Time Limit Exceeded for huge amount of data
 */
 
 #include <bits/stdc++.h>
