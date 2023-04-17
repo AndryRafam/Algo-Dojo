d1250acf2f1a9bfbf3fd2b125498d37adce45c3f
Andry Rafam <andryrafam@protonmail.com>
2023-04-17 21:37:17 +0300

    Update Longest_Palindromic_Substring.cs

--- String/Longest_Palindromic_Substring.cs
+++ String/Longest_Palindromic_Substring.cs
@@ -1,39 +1,68 @@
-/*Longest palindromic substring.
-Given a string s, return the longest palindromic substring in s.
-Brute force approach.
+/*
+Given a string s, find the length of the longest substring without repeating characters.
 
-Time complexity: O(N³), where N is the length of the string.*/
+Let N, the length of the input string.
+
+Solution 1: Brute force approach - Time complexity: O(N³).
+
+Solution 2: Sliding window approach - Time complexity: O(N). 
+*/
 
 using System;
+using System.Collections.Generic;
 
-public class Solution{
-    public static string longestPalindrome(string s){
+class Solution_1{
+    public int lengthOfLongestSubstrNoRepeat(string s){
         int max_len = 0;
-        string longest_pal = "";
-        for(int i = 0; i < s.Length; ++i){
-            for(int j = 1; j <= s.Length-i; ++j){
-                string temp = s.Substring(i,j);
-                if(isPalindrome(temp) && max_len < temp.Length){
-                    longest_pal = temp;
+        int l = 1;
+        while(l < s.Length){
+            for(int i = 0; i <= s.Length-l; ++i){
+                string temp = s.Substring(i,l);
+                if(isUnique(temp)){
                     max_len = temp.Length;
                 }
             }
         }
-        return longest_pal;
+        return max_len;
     }
-    private static bool isPalindrome(string s){
+    private static bool isUnique(string a){
+        HashSet<char> hash = new HashSet<char>();
+        foreach(char x in a){
+            hash.Add(x);
+        }
+        if(hash.Count==a.Length){
+            return true;
+        }
+        return false;
+    }
+}
+
+class Solution_2{
+    public int lengthOfLongestSubstrNoRepeat(string s){
+        HashSet<char> hashSet = new HashSet<char>();
         int l = 0;
-        int r = s.Length-1;
-        while(l < r){
-            if(s[l++]!=s[r--]){
-                return false;
+        int r = 0;
+        int max_len = 0;
+        while(r < s.Length){
+            if(!hashSet.Contains(s[r])){
+                hashSet.Add(s[r]);
+                max_len = Math.Max(max_len,r-l+1);
+                r++;
+            }
+            else{
+                hashSet.Remove(s[l]);
+                l++;
             }
         }
-        return true;
+        return max_len;
     }
+}
+
+class Driver{
     public static int Main(){
-        string test = "abbac";
-        Console.Write(longestPalindrome(test));
+        Solution_2 s2 = new Solution_2();
+        string x = "abcabcbb";
+        Console.Write(s2.lengthOfLongestSubstrNoRepeat(x));
         return 0;
     }
 }
