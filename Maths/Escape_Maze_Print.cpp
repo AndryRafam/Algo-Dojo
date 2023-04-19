c28747fea28dd08e5e040af7c2ac5d136d775748
Andry Rafam <andryrafam@protonmail.com>
2023-04-19 17:47:47 +0300

    Update Escape_Maze_Print.cpp

--- Maths/Escape_Maze_Print.cpp
+++ Maths/Escape_Maze_Print.cpp
@@ -11,72 +11,69 @@ using namespace std;
 #define Yellow "\x1B[33m"
 #define Reset "\x1B[0m"
 
-class Solution {
-	public:
-	static bool path(vector<vector<int>> &maze,int x, int y, int tx, int ty){
-		vector<vector<int>> sol(maze);
-		if(dfs(maze,x,y,tx,ty,sol)){
-			printPath(sol);
-			return true;
-		}
-		cout << "NO PATHS";
-		return false;
-	}
-	private:
-	static bool dfs(vector<vector<int>> &maze, int x, int y, int tx, int ty, vector<vector<int>> &sol){
-		int N = maze.size();
-		int M = maze[0].size();
-		vector<int> dx = {-1,0,0,1};
-		vector<int> dy = {0,-1,1,0};
-		if(x < 0 || x >= N || y < 0 || y >= M || maze[x][y]!=0){
-			return false;
-		}
-		if(x == tx && y == ty){
-			sol[x][y]=9;
-			return true;
-		}
-		else if(maze[x][y]==0){
-			maze[x][y]='$';
-			for(auto k(0); k < 4; ++k){
-				if(dfs(maze,x+dx[k],y+dy[k],tx,ty,sol)){
-					sol[x][y]=9;
-					return true;
-				}
-			}
-		}
-		maze[x][y] = 1; // backtrack and unmark the current cell as visited
-		return false;
-	}
-	static void printPath(vector<vector<int>> &maze){
-		for(auto i(0); i < maze.size(); ++i){
-			for(auto j(0); j < maze[0].size(); ++j){
+class Solution{
+    public:
+    static bool path(vector<vector<int>> &maze){
+        //vector<vector<int>> sol(maze); // solution matrix filled with 0, same size as maze
+        if(dfs(maze,0,0/*,sol*/)){
+            printPath(maze);
+            return true;
+        }
+        cout << "NO PATH";
+        return false;
+    }
+    private:
+    static void printPath(vector<vector<int>> &maze){
+        for(auto i(0); i < maze.size(); ++i){
+            for(auto j(0); j < maze[0].size(); ++j){
 				if(maze[i][j]==9){
 					cout << " " << Yellow << maze[i][j] << Reset << " ";
 				}
-				else{
+				else {
 					cout << " " << maze[i][j] << " ";
 				}
-			}
-			cout << "\n";
-		}
-		return;
-	}
+            }
+            cout << "\n\n";
+        }
+        return;
+    }
+    static bool dfs(vector<vector<int>> &maze, int x, int y/*,vector<vector<int>> &sol*/){
+        int N = maze.size();
+        int M = maze[0].size();
+        vector<int> dx = {-1,0,0,1};
+        vector<int> dy = {0,-1,1,0};
+        if(x < 0 || x >= N || y < 0 || y >= M || maze[x][y]!=1){
+            return false;
+        }
+        if(x==N-1 && y==M-1){
+            //sol[x][y]=9; // mark path as 9
+            maze[x][y]=9;
+            return true;
+        }
+        else if(maze[x][y]==1){
+            maze[x][y] = 9;
+            for(auto k(0); k < 4; ++k){
+                if(dfs(maze,x+dx[k],y+dy[k]/*,sol*/)){
+                    //sol[x][y]=9; // mark path as 9
+                    return true;
+                }
+            }
+        }
+        maze[x][y] = 1; // backtrack and unmark the current cell as visited
+        return false;
+    } 
 };
 
 int main(){
-	ios_base::sync_with_stdio(false);
-	cout.tie(0);
-	vector<vector<int>> maze = {{1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-				    {1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1},
-				    {1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1},
-				    {1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1},
-				    {1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1,1,1,1},
-				    {1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1},
-				    {1,0,1,0,1,0,1,1,1,1,1,0,1,0,0,0,1,1,0,1},
-				    {1,0,1,0,1,0,0,0,1,0,0,0,0,0,1,1,1,1,0,1},
-				    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1}};
-	int N = maze.size();
-	int M = maze[0].size();
-	Solution::path(maze,0,1,N-1,M-2);
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cout.tie(0);
+    vector<vector<int>> maze = {{1, 0, 0, 0, 0},
+				{1, 0, 1, 1, 1},
+				{1, 1, 1, 0, 1},
+				{0, 0, 0, 1, 1},
+				{1, 1, 0, 1, 0},
+				{0, 0, 0, 1, 1}};
+    cout << "\n";
+    Solution::path(maze);
+    return 0;
 }
