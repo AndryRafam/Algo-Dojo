28f4f91d3135caa798fb76652bebd06521cf974c
Andry Rafam <andryrafam@protonmail.com>
2023-04-27 19:40:01 +0300

    Update Word_Search_3.cpp

--- String/Word_Search_3.cpp
+++ String/Word_Search_3.cpp
@@ -60,45 +60,6 @@ class Solution1{
     }
 };
 
-/*Iterative DFS
-class Solution2{
-    private:
-    static bool isValid(vector<vector<char>> &grid, string word, int x, int y, int k){
-        int m = grid.size();
-        int n = grid[0].size();
-        if(x < 0 or x >= m or y < 0 or y >= n or grid[x][y]!=word[k]){
-            return false;
-        }
-        return true;
-    }
-    public:
-    static bool DFS(vector<vector<char>> &grid, string word, pair<pair<int,int>,int> start){
-        vector<vector<bool>> visited(grid.size(),vector<bool>(grid[0].size(),false));
-        stack<pair<pair<int,int>,int>> st;
-        st.push(start);
-        visited[start.first.first][start.first.second] = true;
-        while(!st.empty()){
-            auto cur = st.top();
-            st.pop();
-            if(cur.second==word.length()){
-                return true;
-            }
-            vector<int> dx = {-1,0,0,1};
-            vector<int> dy = {0,-1,1,0};
-            for(auto k(0); k < 4; ++k){
-                int newX = cur.first.first+dx[k];
-                int newY = cur.first.second+dy[k];
-                if(isValid(grid,word,newX,newY,cur.second) and !visited[newX][newY]){
-                    visited[newX][newY] = true;
-                    pair<pair<int,int>,int> next = {{newX,newY},cur.second+1};
-                    st.push(next);
-                }
-            }
-        }
-        return false;
-    }
-};*/
-
 int main(){
     ios_base::sync_with_stdio(false);
     vector<vector<char>> grid{{'A', 'B', 'C', 'E'},
@@ -108,33 +69,6 @@ int main(){
     for(auto &x : Solution1::wordSearch(grid,words)){
         cout << x << " ";
     }
-
-    /*vector<vector<char>> board = {{'o','a','a','n'},
-                                  {'e','t','a','e'},
-                                  {'i','h','k','r'}, 
-                                  {'i','f','l','v'}};
-
-    vector<string> words = {"oath","pea","eat","rain"};
-    unordered_set<string> ans;
-    for(auto i(0); i < board.size(); ++i){
-        for(auto j(0); j < board[0].size(); ++j){
-            for(auto &word : words){
-                pair<pair<int,int>,int> start = {{i,j},0};
-                if(Solution2::DFS(board,word,start)){
-                    ans.insert(word);
-                }
-            }
-        }
-    }
-    if(ans.empty()){
-        cout << "NOT FOUND" << endl;
-    }
-    else{
-        for(auto &x : ans){
-            cout << x << " ";
-        }
-    }
-    return 0;*/
-
+    
     return 0;
 }
