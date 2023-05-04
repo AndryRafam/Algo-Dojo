7d3c7b00535220cb68940b46f58fe7e65b702a07
Andry Rafam <andryrafam@protonmail.com>
2023-05-04 18:40:44 +0300

    Update Number_of_Islands.cpp

--- Maths/Number_of_Islands.cpp
+++ Maths/Number_of_Islands.cpp
@@ -10,48 +10,54 @@ using namespace std;
 
 class Solution{
     public:
-    static int numIslands(vector<vector<char>> &grid){
+    static int numIslands(vector<vector<int>> &grid){
         int count = 0;
+        int color = 2;
         for(auto i(0); i < grid.size(); ++i){
             for(auto j(0); j < grid[0].size(); ++j){
-                if(grid[i][j]=='1'){
+                if(grid[i][j]==1){
                     count++;
-                    dfs(grid,i,j);
+                    dfs(grid,i,j,color);
+                    color++;
                 }
             }
         }
         return count;
     }
     private:
-    static bool dfs(vector<vector<char>> &grid, int i, int j){
+    static void dfs(vector<vector<int>> &grid, int i, int j, int color){
         int N = grid.size();
         int M = grid[0].size();
-        if(i < 0 || i >= N || j < 0 || j >= M || grid[i][j]=='0'){
-            return false;
+        if(i < 0 || i >= N || j < 0 || j >= M || grid[i][j]==0){
+            return;
         }
         vector<pair<int,int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        if(grid[i][j]=='1'){
-            grid[i][j]='0';
+        if(grid[i][j]==1){
+            grid[i][j]=color;
             for(auto &dir : directions){
                 int new_row = i+dir.first;
                 int new_col = j+dir.second;
-                if(dfs(grid,new_row,new_col)){
-                    return true;
-                }
+                dfs(grid,new_row,new_col,color);
             }
         }
-        return false;
+        return;
     }
 };
 
 int main(){
     ios_base::sync_with_stdio(false);
-    vector<vector<char>> grid = {
-        {'1', '1', '0', '0', '0'},
-        {'1', '1', '0', '0', '0'},
-        {'0', '0', '1', '0', '0'},
-        {'0', '0', '0', '1', '1'}
+    vector<vector<int>> grid = {
+        {1, 1, 0, 0, 0},
+        {1, 1, 0, 0, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 1, 1}
     };
-    cout << Solution::numIslands(grid) << "\n";
+    cout << Solution::numIslands(grid) << "\n\n";
+    for(auto row : grid){
+        for(auto cell : row){
+            cout << cell << " ";
+        }
+        cout << "\n";
+    }
     return 0;
 }
