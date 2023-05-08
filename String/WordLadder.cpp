537e0f2940d4fc934a5ee603204bf26c31c4493d
Andry Rafam <andryrafam@protonmail.com>
2023-05-08 21:26:28 +0300

    Update WordLadder.cpp

--- String/WordLadder.cpp
+++ String/WordLadder.cpp
@@ -1,8 +1,14 @@
 /*Word Ladder:
-You are given a dictionary of words and a start word. Your task is to transform the start word into the target word
-by changing exactly one character at a time. At each step, the intermediate word must be in the dictionary.
-Find the minimum number of steps required to transform the start word into the target word. 
-If it's not possible, return -1.
+
+A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence 
+of words beginWord -> s1 -> s2 -> ... -> sk such that:
+
+    Every adjacent pair of words differs by a single letter.
+    Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+    sk == endWord
+
+Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest
+transformation sequence from beginWord to endWord, or -1 if no such sequence exists.
 */
 
 #include <bits/stdc++.h>
