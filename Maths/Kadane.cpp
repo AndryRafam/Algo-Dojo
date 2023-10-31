425bbbc3c16a40c7511c6c819b68750bc2fdef98
Andry R <andryrafam@protonmail.com>
2023-10-31 18:40:50 +0300

    Update Kadane.cpp

--- Maths/Kadane.cpp
+++ Maths/Kadane.cpp
@@ -6,23 +6,18 @@ Time complexity: O(N), where N is the size of the array.
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
-    public:
-    static int kadane(vector<int> &arr){
-        int currSum = arr[0];
-        int maxSum = arr[0];
-        
-        for(auto i(1); i < arr.size(); ++i){
-            currSum = max(arr[i],currSum+arr[i]);
-            maxSum = max(currSum,maxSum);
-        }
-        return maxSum;
+int kadane(vector<int> arr){
+    int currSum = arr[0];
+    int maxSum = arr[0];
+    for(int i(1); i < arr.size(); ++i){
+        currSum = max(arr[i],currSum+arr[i]);
+        maxSum = max(currSum,maxSum);
     }
-};
+    return maxSum;
+}
 
 int main(){
-    ios_base::sync_with_stdio(false);
     vector<int> arr = {-1,2,4,-3,5,2,-5,2};
-    cout << Solution::kadane(arr) << endl;
+    printf("%d\n",kadane(arr));
     return 0;
 }
