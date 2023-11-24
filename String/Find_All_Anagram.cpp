cc8c1cef6f7b05b40d0946219cbd89d7e9ae23c9
Andry R <andryrafam@protonmail.com>
2023-11-24 16:46:13 +0300

    Update Find_All_Anagram.cpp

--- String/Find_All_Anagram.cpp
+++ String/Find_All_Anagram.cpp
@@ -1,49 +1,49 @@
 /*
-Given two strings s and p, return an array of all the start indices of p's anagrams in s. 
-You may return the answer in any order.
-An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
-typically using all the original letters exactly once.
+Hidden Anagram: https://edabit.com/challenge/vxWGJmtjpWtu36xfj
+Create a function that takes two strings. The first string contains a sentence containing the 
+letters of the second string in a consecutive sequence but in a different order. The hidden
+anagram must contain all the letters, including duplicates, from the second string in any
+order and must not contain any other alphabetic characters.
+Write a function to find the anagram of the second string embedded somewhere in the first
+string. You should ignore character case, any spaces, and punctuation marks and return the
+anagram as a lower case string with no spaces or punctuation marks.
 
-Time complexity: O(N) using sliding window techniques, where N is the length of s.
+Time Complexity: O(N) using sliding window, where N is the length of the first string.
 */
-
 
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-    public:
-    static vector<int> findAnagrams (string s, string p) {
-        if(s.length() < p.length()) {
-            return {};
-        }
-        vector<int> res;
-        vector<int> hashS(26,0);
-        vector<int> hashP(26,0);
-        for(auto x(0); x < p.length(); ++x) {
-            hashS[s[x]-'a']++;
-            hashP[p[x]-'a']++;
-        }
-        if(hashS == hashP){
-            res.emplace_back(0);
-        }
-        for(auto i(p.length()); i < s.length(); ++i) {
-            hashS[s[i]-'a']++;
-            hashS[s[i-p.length()]-'a']--;
-            if(hashS == hashP) {
-                res.emplace_back(i-p.length()+1);
-            }
-        }
-        return res;
+vector<int> findAnagrams(string s, string p){
+    if(s.length() < p.length()){
+        return {};
     }
-};
+    vector<int> res;
+    vector<int> hashS(26,0);
+    vector<int> hashP(26,0);
+    for(int i = 0; i < p.length(); ++i){
+        hashS[s[i]-'a']++;
+        hashP[p[i]-'a']++;
+    }
+    if(hashS==hashP){
+        res.emplace_back(0);
+    }
+    for(int j = p.length(); j < s.length(); ++j){
+        hashS[s[j]-'a']++;
+        hashS[s[j-p.length()]-'a']--;
+        if(hashS==hashP){
+            res.emplace_back(j-p.length()+1);
+        }
+    }
+    return res;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     string s, p;
     cin >> s >> p;
-    for(auto x : Solution::findAnagrams(s,p)) {
-        cout << x << " ";
+    for(auto x : findAnagrams(s,p)){
+        printf("%d ",x);
     }
     return 0;
 }
