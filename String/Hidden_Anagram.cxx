5ddc405e8ebb1c495eaa1648ebaea95e8ff67952
Andry R <andryrafam@protonmail.com>
2023-11-24 17:07:44 +0300

    Update Hidden_Anagram.cxx

--- String/Hidden_Anagram.cxx
+++ String/Hidden_Anagram.cxx
@@ -17,49 +17,44 @@ Time Complexity: O(N) using sliding window, where N is the length of the first s
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-    public:
-    static string hiddenAnagram(string a, string b) {
-        if(a.length() < b.length()) {
-            return "Not found";
+string ignore_others(string s){
+    string t = "";
+    for(auto x :s){
+        if((int(x)>=65&&int(x)<=90)||(int(x)>=97&&int(x)<=122)){
+                t+=tolower(x);   
         }
-        string x = ignore_others(a).c_str(); 
-        string y = ignore_others(b).c_str();
+    }
+    return t;
+}
 
-        vector<int> res;
-        vector<int> countX(26,0);
-        vector<int> countY(26,0);
-        for(auto i(0); i < y.length(); ++i) {
-            countX[x[i]-'a']++;
-            countY[y[i]-'a']++;
-        }
-        if(countX == countY) {
-            res.emplace_back(0);
-        }
-        for(auto i(y.length()); i < x.length(); ++i) {
-            countX[x[i]-'a']++;
-            countX[x[i-y.length()]-'a']--;
-            if(countX == countY) {
-                res.emplace_back(i-y.length()+1);
-            }
-        }
-        if(res.empty()) {
-            return "Not found";
-        }
-        return x.substr(res[0],y.length());
+string hiddenAnagram(string a, string b){
+    if(a.length() < b.length()){
+        return "not found";
     }
-    private:
-    static string ignore_others(string s) { // function to ignore all characters but lower ones.
-        string t = "";
-        for(auto x : s) {
-            if((int(x)>=65&&int(x)<=90)||(int(x)>=97&&int(x)<=122))
-                t+=tolower(x);
-            else
-                t+="";    
-        }
-        return t;
+    string x = ignore_others(a).c_str();
+    string y = ignore_others(b).c_str();
+    vector<int> res;
+    vector<int> countX(26,0);
+    vector<int> countY(26,0);
+    for(int i = 0; i < y.length(); ++i){
+        countX[x[i]-'a']++;
+        countY[y[i]-'a']++;
     }
-};
+    if(countX==countY){
+        res.emplace_back(0);
+    }
+    for(int i = y.length(); i < x.length(); ++i){
+        countX[x[i]-'a']++;
+        countX[x[i-y.length()]-'a']--;
+        if(countX==countY){
+            res.emplace_back(i-y.length()+1);
+        }
+    }
+    if(res.empty()){
+        return "not found";
+    }
+    return x.substr(res[0],y.length());
+}
 
 int main(){
     string a = "D  e b90it->?$ (c)a r...d,,#~";
@@ -68,8 +63,8 @@ int main(){
     string s = "An old west action hero actor";
     string t = "Clint Eastwood";
     
-    cout << Solution::hiddenAnagram(a,b) << "\n"; // debitcard
-    cout << Solution::hiddenAnagram(s,t); // noldwestactio
+    cout << hiddenAnagram(a,b) << "\n"; // debitcard
+    cout << hiddenAnagram(s,t); // noldwestactio
 
     return 0;
 }
