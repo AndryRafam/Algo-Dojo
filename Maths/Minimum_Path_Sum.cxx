fe0ee9663417c1647e5c73c2128e6337e72be912
Andry R <andryrafam@protonmail.com>
2023-11-25 22:51:39 +0300

    Update Minimum_Path_Sum.cxx

--- Maths/Minimum_Path_Sum.cxx
+++ Maths/Minimum_Path_Sum.cxx
@@ -9,34 +9,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
-	public:
-	static int minPathSum(vector<vector<int>> &grid){
-		int M = grid.size();
-		int N = grid[0].size();
-		vector<vector<int>> dp(M, vector<int> (N,0));
-		dp[0][0] = grid[0][0];
-		
-		//fill the 1st row
-		for(int j(1); j < M; ++j){
-			dp[0][j] = dp[0][j-1]+grid[0][j];
-		}
-		//fill the 1st column
-		for(int i(1); i < N; ++i){
-			dp[i][0] = dp[i-1][0]+grid[i][0];
-		}
-		for(int i(1); i < M; ++i){
-			for(int j(1); j < N; ++j){
-				dp[i][j] = grid[i][j] + min(dp[i-1][j],dp[i][j-1]);
-			}
-		}
-		return dp[M-1][N-1];
+int minPathSum(vector<vector<int>> grid){
+	int m = grid.size();
+	int n = grid[0].size();
+	vector<vector<int>> dp(m,vector<int>(n,0));
+	dp[0][0]=grid[0][0];
+	// fill the 1st row
+	for(size_t j = 1; j < m; ++j){
+		dp[0][j]=dp[0][j-1]+grid[0][j];
 	}
-};
+	// fill the 1st column
+	for(size_t i = 1; i < n; ++i){
+		dp[i][0]=dp[i-1][0]+grid[i][0];
+	}
+	for(size_t i = 1; i < m; ++i){
+		for(size_t j = 1; j < n; ++j){
+			dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1]);
+		}
+	}
+	return dp[m-1][n-1];
+}
 
 int main(){
 	ios_base::sync_with_stdio(false);
 	vector<vector<int>> grid = {{1,3,1},{1,5,1},{4,2,1}};
-	cout << Solution::minPathSum(grid);
+	cout << minPathSum(grid);
 	return 0;
 }
