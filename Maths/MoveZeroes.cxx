d6a98e8a1627cdd5c5107394d2db5e959f0c1804
Andry R <andryrafam@protonmail.com>
2023-12-11 18:27:17 +0300

    Update MoveZeroes.cxx

--- Maths/MoveZeroes.cxx
+++ Maths/MoveZeroes.cxx
@@ -1,1 +1,33 @@
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
+Note that you must do this in-place without making a copy of the array.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void moveZeroes(vector<int> &nums) {
+    int n = nums.size();
+    int next = 0;
+    for(auto x : nums) {
+        if(x!=0) {
+            nums[next]=x;
+            next++;
+        }
+    }
+    for(auto i = next; i < n; ++i) {
+        nums[i]=0;
+    }
+    return;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    vector<int> nums = {0,1,0,3,12};
+    moveZeroes(nums);
+    for(auto y : nums){
+        cout << y << " ";
+    }
+    return 0;
+}
