223d5ae0686b4e73a91c657ddfb210b83464609f
Andry R <andryrafam@protonmail.com>
2023-12-28 16:08:04 +0300

    Update and rename peak_element.cpp to PeakElement.cpp

---
+++ Element/PeakElement.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+    public:
+    static int peakElement(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size()-1;
+        while(l < r) {
+            int m = (l+r) >> 1;
+            if(nums[m]>=nums[m+1]) {
+                r = m;
+            } else {
+                l = m+1;
+            }
+        }
+        return l;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    vector<int> nums = {1,2,1,3,5,6,4};
+    cout << Solution::peakElement(nums);
+    return 0;
+}
