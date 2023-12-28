d0e53a6e9af4ab537444f3fc4f7d93f99cc694e1
Andry R <andryrafam@protonmail.com>
2023-12-28 13:42:15 +0300

    Update Duplicate.cxx

--- ByteByByte/Duplicate.cxx
+++ ByteByByte/Duplicate.cxx
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+using namespace std;
 
+vector<int> findDuplicates(vector<int> &arr) {
+    unordered_set<int> hashset;
+    vector<int> res;
+    for(auto &x : arr) {
+        if(hashset.find(x)!=hashset.end()) {
+            res.push_back(x);
+        }
+        hashset.insert(x);
+    }
+    return res;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    vector<int> test = {1,2,2};
+    for(auto &y : findDuplicates(test)) {
+        cout << y << " ";
+    }
+    return 0;
+}
