2a7c14c4f9e47526dd40fb6ee002eab855d5d2bb
Andry R <andryrafam@protonmail.com>
2023-12-29 13:06:10 +0300

    Update WordBreak.cxx

--- String/WordBreak.cxx
+++ String/WordBreak.cxx
@@ -12,6 +12,7 @@ class Solution {
         const int n = s.length();
         unordered_set<string> wordSet({wordDict.begin(),wordDict.end()});
         vector<bool> dp(n+1);
+        dp[0]=true;
         for(int i = 1; i <= n; ++i) {
             for(int j = 0; j < i; ++j) {
                 if(dp[j] && wordSet.count(s.substr(j,i-j))) {
