1a5fb0ae51acc6cf1c83ddeebe39380b31ff39d5
Andry R <41973268+AndryRafam@users.noreply.github.com>
2024-02-11 19:14:23 +0300

    Add files via upload

---
+++ String/SmallestSubstringOfAll.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false)
+#define ust unordered_set
+
+class Solution {
+    public:
+    static string getShortestUniqueSubstring(vector<char> arr, string str) {
+        vector<string> res;
+        string t = "";
+        for(size_t i = 0; i < str.length(); ++i) {
+            for(size_t j = arr.size(); j <= str.length()-i; ++j) {
+                t = str.substr(i,j);
+                if(isUnique(t)==1) {
+                    return t;
+                }
+            }
+        }
+        return "";
+    }
+    private:
+    static bool isUnique(const string str) {
+        ust <char> freq;
+        for(auto c : str) {
+            if(freq.find(c)!=freq.end()) {
+                return false;
+            }
+            freq.insert(c);
+        }
+        return true;
+    }
+};
+
+int main() {
+    fast_io;
+    cin.tie(0);
+    vector<char> arr = {'x','y','z'};
+    string str; cin >> str;
+    cout << Solution::getShortestUniqueSubstring(arr,str);
+    return 0;
+}
