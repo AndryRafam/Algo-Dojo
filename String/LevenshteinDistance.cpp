ffacf3759890eb83482f2a750b34aa8880e9ab75
Andry R <41973268+AndryRafam@users.noreply.github.com>
2024-02-12 18:30:21 +0300

    Update LevenshteinDistance.cpp

--- String/LevenshteinDistance.cpp
+++ String/LevenshteinDistance.cpp
@@ -3,10 +3,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define fast_io ios::sync_with_stdio(false)
+
 int levenshteinDistance(const string &s1, const string &s2) {
 	int m = s1.length();
 	int n = s2.length();
-	int cost;
 	int dp[m+1][n+1];
 	for(int i = 0; i <= m; ++i) {
 		dp[i][0]=i;
@@ -16,15 +17,14 @@ int levenshteinDistance(const string &s1, const string &s2) {
 	}
 	for(int i = 1; i <= m; ++i) {
 		for(int j = 1; j <= n; ++j) {
-			cost = (s1[i-1]==s2[j-1]) ? 0:1;
-			dp[i][j]=min({dp[i][j-1]+1,dp[i-1][j]+1,dp[i-1][j-1]+cost});
+			dp[i][j]=min({dp[i][j-1]+1,dp[i-1][j]+1,dp[i-1][j-1]+(s1[i-1]==s2[j-1] ? 0:1)});
 		}
 	}
 	return dp[m][n];
 }
 
 int main() {
-	ios::sync_with_stdio(false);
+	fast_io;
 	cin.tie(0);
 	string s; cin >> s;
 	string t; cin >> t;
