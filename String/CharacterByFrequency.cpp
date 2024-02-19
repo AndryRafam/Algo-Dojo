4820184142eed345a498cfc48cb2797770f0b84f
Andry R <41973268+AndryRafam@users.noreply.github.com>
2024-02-20 00:50:46 +0300

    Update CharacterByFrequency.cpp

--- String/CharacterByFrequency.cpp
+++ String/CharacterByFrequency.cpp
@@ -2,6 +2,8 @@
 Given a string s, sort it in decreasing order based on the frequency of the characters.
 The frequency of a character is the number of times it appears in the string.
 Return the sorted string. If there are multiple answers, return any of them.
+
+Time complexity: O(N*logN), N is the size of the string.
 */
 
 #include <bits/stdc++.h>
